6bdfee2ba59b30659b92281dcaad5da993effb78
Lwolwo <zuihouyiye0704@126.com>
2020-08-14 16:52:29 +0800

    feat: use interface get /product

--- src/component/card/Card.js
+++ src/component/card/Card.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+
 import './Card.css'
 
 export default class Card extends Component {
   render () {
+    const {name, price, type, picUrl} = this.props;
     return (
       <div className="card">
+        {/* <img src={require(picUrl)} alt="pic" /> */}
         <img src={require("../../asserts/1.jpg")} alt="pic" />
-        <p className="name">可乐1</p>
-        <p className="price">单价：1元/瓶</p>
+        <p className="name">{name}</p>
+        <p className="price">单价：{price}元/{type}</p>
         <button className="button">+</button>
       </div>
     )
--- src/component/store/Store.js
+++ src/component/store/Store.js
@@ -3,16 +3,37 @@ import './Store.css'
 import Card from '../card/Card'
 
 export default class Store extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    }
+  }
+
+  async componentDidMount () {
+    const result = await fetch('http://localhost:8080/product', {
+      method: 'GET',
+    }).then((response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json()
+      }
+      return Promise.reject();
+    });
+    this.setState({
+      products: result
+    })
+  }
+
+  render () {
+    const { products } = this.state;
     return (
       <div className="store">
         <div className="products">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            products.map((item, index) =>
+              <Card key={index} name={item.name} price={item.price} type={item.type} picUrl={item.picUrl} />
+            )
+          }
         </div>
       </div>
     )
